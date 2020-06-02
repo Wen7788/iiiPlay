ddf8c42a8a6db6ce6e7b1e5c21e07a0a21db18b4
eric406 <65290154+eric406@users.noreply.github.com>
2020-06-02 13:54:12 +0800

    0602 13:55
    0602 13:55

--- src/com/iiiproject/lab02/controller/ResetPasswordServlet.java
+++ src/com/iiiproject/lab02/controller/ResetPasswordServlet.java
@@ -54,9 +54,9 @@ public class ResetPasswordServlet{
         			return "member/message";
         }
  
-        String content = "Hi, this is your new password: " + newPassword;
-        content += "\nNote: for security reason, "
-                + "you must change your password after logging in.";
+        String content = "你好，這是您的新密碼: " + newPassword;
+        content += "\nNote: 為了安全理由， "
+                + "登入後請修改密碼，此由iiiPlay系統自動發送，請勿回覆";
  
  
         try {
